ab511a802155e3fd32988c7d71447db6a0ec7b57
Alexey Barashkov <lnikell@gmail.com>
2015-09-24 23:36:00 +0500

    working on next version

--- css_to_sass.py
+++ css_to_sass.py
@@ -2,6 +2,16 @@ import sublime, sublime_plugin
 import re
 
 class CssToSass(sublime_plugin.TextCommand):
+  options = {
+    'indent': 2,
+    'openingBracket': '{',
+    'closingBracket': '}',
+    'semicolon': ':',
+    'eol': ';',
+    'unPrefix': False,
+    'keepColons': False,
+  }
+
   def run(self, edit):
     if self.view.file_name() and self.view.file_name().endswith(".sass"):
       self.convert(sublime.get_clipboard())
@@ -11,12 +21,33 @@ class CssToSass(sublime_plugin.TextCommand):
 
   def convert(self, text):
       if (";" in text):
-        self.clean()
+        self.process()
         self.view.run_command('paste_and_indent')
       else:
         self.view.run_command('paste')
 
-  def clean(self):
+  def process(self):
     text = sublime.get_clipboard()
-    text = re.sub("(;|{|})", "", text)
-    sublime.set_clipboard(text)
+    # text = re.sub("(;|{|})", "", text)
+    # sublime.set_clipboard(text)
+    
+    tree = {'children': {}}
+    # Remove comments
+    text = re.sub("\/\*[\s\S]*?\*\/", "", text)
+    # Process each css block
+    for (selector, declaration) in re.findall("([^{]+)\{([^}]+)\}", text):
+      selectors = []
+      path = tree
+
+      selector = selector.strip()
+      if re.search(",", selector):
+        self.addRule(path, selector)
+      
+ 
+  def addRule(self, path, selector):
+    print(path)
+    path['children'][selector] = path['children'][selector] | { children: 1, declarations: [] }
+    print(path)
+
+
+      
